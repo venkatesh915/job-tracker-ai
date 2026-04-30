b9d0e7ecbabdbe4b8644fac39a5b3df2acd021df
Boddu Venkateswara Rao <venkateshboddu923@gmail.com>
2026-04-30 13:45:13 +0530

    Update Job.js

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 
-const jobSchema = new mongoose.Schema({
-  title: String,
-  company: String,
-  status: {
-    type: String,
-    enum: ["applied", "interview", "rejected"],
-    default: "applied",
+const jobSchema = new mongoose.Schema(
+  {
+    title: String,
+    company: String,
+    status: {
+      type: String,
+      default: "applied"
+    }
   },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Job", jobSchema);
